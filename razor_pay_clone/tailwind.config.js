3fcb48ffed055af122121af5068e94e17188893c
harshbhar0629 <harshbhar.0629@gmail.com>
2024-08-02 11:37:08 +0530

    Clone completed:

--- razor_pay_clone/tailwind.config.js
+++ razor_pay_clone/tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
         mullish: ["sans-serif"]
       },
       colors: {
+        
         deepBlue: "#02042a",
         lightBlue: "#2b84ea",
         lightBlue300: "#4b94ed",
